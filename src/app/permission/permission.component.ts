f90a857168ab031e3166301e302e602fa6fb516f
AleDulce13 <5323100074@correcaminos.uts.edu.mx>
2025-04-05 11:52:13 -0700

    hola

--- src/app/permission/permission.component.ts
+++ src/app/permission/permission.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PermissionService } from '../services/permission.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from '../header/header.component';
 import { User } from './user.model';
 
@@ -13,15 +13,19 @@ import { User } from './user.model';
   styleUrl: './permission.component.css'
   
 })
-export class PermissionComponent {
+export class PermissionComponent implements OnInit {
   permissions: any[] = [];
   searchTerm: string = ''; // Para la búsqueda
   filteredPermissions: any[] = [];
 
-  constructor(private permisosService: PermissionService) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.permisosService.getPermisos().subscribe(
+    this.getPermisos();
+  }
+
+  getPermisos(): void {
+    this.http.get<any>('https://localhost:7159/api/Permisos/Get').subscribe(
       data => {
         this.permissions = data;  // Asignamos los datos de la API a la propiedad
         this.filteredPermissions = this.permissions;
@@ -32,14 +36,12 @@ export class PermissionComponent {
     );
   }
 
-  // Método para filtrar permisos según el término de búsqueda
   filterPermissions(): void {
     this.filteredPermissions = this.permissions.filter(perm => 
       perm.usuarioId.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
   }
 
-  // Limpiar la búsqueda
   clearSearch(): void {
     this.searchTerm = '';
     this.filterPermissions();
